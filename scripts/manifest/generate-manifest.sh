dff817dee0fe095b250991abbc87ae383e0c6df9
Olivier ORAND <olivier.orand@orange.com>
2017-02-23 22:18:59 +0100

    scripts(manifest): customize template file selection

--- scripts/manifest/generate-manifest.sh
+++ scripts/manifest/generate-manifest.sh
@@ -4,12 +4,15 @@ spruce --version
 CURRENT_DIR=$(pwd)
 OUTPUT_DIR=${CURRENT_DIR}/generated-files/
 
-SUFFIX=-tpl.yml
+SUFFIX=${SUFFIX:-"-tpl.yml"}
+echo "selecting ${SUFFIX} in ${YML_TEMPLATE_DIR}"
 for template in $(ls $YML_TEMPLATE_DIR/*$SUFFIX)
 do
     filename=$(basename $template)
-    echo "Processing $filename"
-    output_filename=${filename%$SUFFIX}.yml
+    file_extention=${SUFFIX#-tpl}
+    echo "processing $filename"
+    output_filename=${filename%$SUFFIX}${file_extention}
+    echo "generating ${output_filename}"
     scripts-resource/scripts/manifest/spruce-manifest.sh $template ${YML_FILES} >${OUTPUT_DIR}/${output_filename}
 done
 
