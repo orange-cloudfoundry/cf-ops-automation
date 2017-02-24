4f55b34c90be1fe68c2376ca5884e8a7df1d06c5
Olivier ORAND <olivier.orand@orange.com>
2017-02-24 11:40:47 +0100

    scripts(manifest): init script for simple post-generation step (see restrictions)
    Restrictions: as the post-generation script is executed in the same docker image running spruce, no spiff is available.
    
    [skip ci]

---
+++ scripts/manifest/manifest-lifecycle-generation.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CURRENT_DIR=$(pwd)
+OUTPUT_DIR=${OUTPUT_DIR:-${CURRENT_DIR}/generated-files/}
+COMMON_SCRIPT_DIR=${COMMON_SCRIPT_DIR:-scripts-resource/scripts/manifest}
+
+${COMMON_SCRIPT_DIR}/generate-manifest.sh
+
+if [ -n "$CUSTOM_SCRIPT_DIR" -a  -f "$CUSTOM_SCRIPT_DIR/post-generate.sh" ]
+then
+    echo "post generation script detected"
+    GENERATE_DIR=$OUTPUT_DIR BASE_TEMPLATE_DIR=$CUSTOM_SCRIPT_DIR $CUSTOM_SCRIPT_DIR/post-generate.sh
+fi
