439ebd4546c81ca5cbdbc530239ccbef4adc50ef
Olivier ORAND <olivier.orand@orange.com>
2017-02-24 14:06:28 +0100

    scripts(manifest): spiff processing (pre-bosh-deploy) have to copy previously generated resources to it own generated resources

--- scripts/manifest/spiff-processing.sh
+++ scripts/manifest/spiff-processing.sh
@@ -5,9 +5,10 @@ OUTPUT_DIR=${OUTPUT_DIR:-${CURRENT_DIR}/generated-files/}
 COMMON_SCRIPT_DIR=${COMMON_SCRIPT_DIR:-scripts-resource/scripts/manifest}
 
 
+cp -r additional-resource/. $OUTPUT_DIR
+
 if [ -n "$CUSTOM_SCRIPT_DIR" -a  -f "$CUSTOM_SCRIPT_DIR/pre-bosh-deploy.sh" ]
 then
-    cp -r additional-resource/. $OUTPUT_DIR
     echo "pre bosh deploy script detected"
     GENERATE_DIR=$OUTPUT_DIR BASE_TEMPLATE_DIR=$CUSTOM_SCRIPT_DIR $CUSTOM_SCRIPT_DIR/pre-bosh-deploy.sh
 fi
