b32f887e6e67a08cfa6a23d40ddbc56e1bb4d8bc
Olivier ORAND <olivier.orand@orange.com>
2017-03-24 18:08:36 +0100

    pipeline(depls): update deploy.sh to use bosh-cli-v2 syntax
    These var env are available in deploy.sh script:
      CURRENT_DEPLS:
      COMMON_SCRIPT_DIR:
      SECRETS_DIR:
      BOSH_USERNAME:
      BOSH_PASSWORD:
      BOSH_TARGET:

---
+++ scripts/bosh_cli_v2_login.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+BOSH_HOST=$1
+BOSH_CLIENT=$2
+BOSH_CLIENT_SECRET=$3
+BOSH_CERT_FILE=$4
+
+
+echo "targeting ${BOSH_HOST}"
+
+DIRECTOR_IP=$(URL_WITHOUT_PORT=${BOSH_HOST%%:25555} nslookup ${URL_WITHOUT_PORT##https://} 2>/dev/null|grep Address|cut -d':' -f2)
+
+if [ -n "$BOSH_CERT_FILE" ]
+then
+  CA_CERT_FILE="--ca-cert=${BOSH_CERT_FILE}"
+fi
+
+bosh alias-env -e $DIRECTOR_IP $CA_CERT_FILE ${BOSH_HOST}
+export BOSH_ENVIRONMENT=${BOSH_TARGET}
