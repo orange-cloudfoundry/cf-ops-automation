756e030af69250fcde32485fd0fdba61d9ec5f89
Olivier ORAND <olivier.orand@orange.com>
2017-04-13 11:05:29 +0200

    feature-pipeline(depls): post bosh deploy support
    if template directory contains post-bosh-deploy.sh, then this  script
    is executed after bosh deployement. The script is executed in the same
    container as the cf push. It is possible to use curl, open ssl and
    basic shell cmd.

---
+++ scripts/post-bosh-deploy-launcher.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+CURRENT_DIR=$(pwd)
+OUTPUT_DIR=${OUTPUT_DIR:-${CURRENT_DIR}/generated-files}
+COMMON_SCRIPT_DIR=${COMMON_SCRIPT_DIR:-scripts-resource/scripts}
+SECRETS_DIR=${SECRETS_DIR:-credentials-resource}
+
+${COMMON_SCRIPT_DIR}/generate-manifest.sh
+
+if [ -n "$CUSTOM_SCRIPT_DIR" -a  -f "$CUSTOM_SCRIPT_DIR/post-bosh-deploy.sh" ]
+then
+    echo "post bosh deploy script detected"
+    chmod +x $CUSTOM_SCRIPT_DIR/post-bosh-deploy.sh
+    GENERATE_DIR=${OUTPUT_DIR} BASE_TEMPLATE_DIR=${CUSTOM_SCRIPT_DIR} ${CUSTOM_SCRIPT_DIR}/post-bosh-deploy.sh
+fi
