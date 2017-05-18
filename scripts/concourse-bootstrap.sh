b49a6e219abe2992cdce2f075c10bcf6efdfc841
Olivier ORAND <olivier.orand@orange.com>
2017-05-18 17:14:00 +0200

    concourse: add a bootstrap script

---
+++ scripts/concourse-bootstrap.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+SECRETS=${SECRETS:-../preprod-secrets}
+FLY_TARGET=${FLY_TARGET:-cw-pp-micro}
+DEPLS_LIST=${DEPLS_LIST:-"micro-depls master-depls ops-depls expe-depls"}
+SKIP_TRIGGER=${SKIP_TRIGGER:=false}
+DEBUG=${DEBUG:=false}
+
+
+usage(){
+    echo "$0" 1>&2
+    echo -e "No parameter supported. Use environment variables:" 1>&2
+    echo -e "\t FLY_TARGET: fly target name to use. DEFAULT: $FLY_TARGET " 1>&2
+    echo -e "\t SECRETS: path to secrets directory. DEFAULT: $SECRETS " 1>&2
+    echo -e "\t DEPLS_LIST: deployments to process. DEFAULT: [$DEPLS_LIST] " 1>&2
+    echo -e "\t SKIP_TRIGGER: skip job triggering after pipeline loading. DEFAULT: [$SKIP_TRIGGER] " 1>&2
+    exit 1
+}
+
+if [ $# -ne 0 ]
+then
+    usage
+fi
+
+SCRIPT_DIR=$(dirname $0)
+SECRET_DIR=$(readlink -e ${SECRETS})
+
+OUTPUT_DIR=$(readlink -f ${SCRIPT_DIR}/boostrap-generated)
+mkdir -p ${OUTPUT_DIR}/pipelines
+
+echo "Deploy on ${FLY_TARGET} using secrets in $SECRET_DIR"
+for depls in ${DEPLS_LIST};do
+    cd ${SCRIPT_DIR}/concourse
+    ./generate-depls.rb -d ${depls} -p ${SECRET_DIR} -o ${OUTPUT_DIR}
+    PIPELINE="${depls}-init-generated"
+    cd ..
+    echo "Load ${PIPELINE} on ${FLY_TARGET}"
+    set +e
+    fly -t ${FLY_TARGET} set-pipeline -p ${PIPELINE} -c ${OUTPUT_DIR}/pipelines/${PIPELINE}.yml  -l ${SECRET_DIR}/micro-depls/concourse-micro/pipelines/credentials-auto-init.yml
+    set -e
+    fly -t ${FLY_TARGET} unpause-pipeline -p ${PIPELINE}
+    if [ "$SKIP_TRIGGER" != "true" ]
+    then
+        fly -t ${FLY_TARGET} trigger-job -j "${PIPELINE}/update-pipeline-${depls}"
+    fi
+done
+
