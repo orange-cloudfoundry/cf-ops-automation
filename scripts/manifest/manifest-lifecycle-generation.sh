26b04ef5ffbb2386fb1c6ded00780da2315ac475
Olivier ORAND <olivier.orand@orange.com>
2017-06-12 17:09:34 +0200

    pipeline(depls): enforce post-generate script can be executed

--- scripts/manifest/manifest-lifecycle-generation.sh
+++ scripts/manifest/manifest-lifecycle-generation.sh
@@ -10,5 +10,6 @@ ${COMMON_SCRIPT_DIR}/generate-manifest.sh
 if [ -n "$CUSTOM_SCRIPT_DIR" -a  -f "$CUSTOM_SCRIPT_DIR/post-generate.sh" ]
 then
     echo "post generation script detected"
+    chmod +x $CUSTOM_SCRIPT_DIR/post-generate.sh
     GENERATE_DIR=$OUTPUT_DIR BASE_TEMPLATE_DIR=$CUSTOM_SCRIPT_DIR $CUSTOM_SCRIPT_DIR/post-generate.sh
 fi
