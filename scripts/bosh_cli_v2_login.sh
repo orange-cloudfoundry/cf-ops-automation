9d690c20486e0637b127ab9a37740952cef95157
Olivier ORAND <olivier.orand@orange.com>
2017-06-14 17:16:23 +0200

    pipeline(depls): se default environment variable for bosh-cli-v2
    bosh_cli_v2_login.sh should only be used with one parameter BOSH_TARGET.
     It still supports BOSH_CERT_FILE as 2nd param.
    bosh_cli_v2_login.sh also removes spaces during hostname to IP conversion
    
    This also impacts deploy.sh scripts too

--- scripts/bosh_cli_v2_login.sh
+++ scripts/bosh_cli_v2_login.sh
@@ -2,15 +2,7 @@
 set -e
 
 BOSH_HOST=$1
-export BOSH_CLIENT=$2
-export BOSH_CLIENT_SECRET=$3
-BOSH_CERT_FILE=$4
-
-
-echo "targeting ${BOSH_HOST}"
-
-DIRECTOR_IP_URL_WITHOUT_PORT=${BOSH_HOST%%:25555}
-DIRECTOR_IP=$(nslookup ${DIRECTOR_IP_URL_WITHOUT_PORT##https://} 2>/dev/null|grep Address|cut -d':' -f2)
+BOSH_CERT_FILE=$2
 
 if [ -n "$BOSH_CERT_FILE" ]
 then
@@ -18,5 +10,39 @@ then
     CA_CERT_FILE="--ca-cert=${BOSH_CERT_FILE}"
 fi
 
-bosh alias-env -e $DIRECTOR_IP $CA_CERT_FILE ${BOSH_HOST}
-export BOSH_ENVIRONMENT=${BOSH_TARGET}
+ERROR_COUNT=0
+if [ -z "$BOSH_CLIENT" ]
+then
+    echo "ERROR: missing environment variable: BOSH_CLIENT"
+    ERROR_COUNT=$(($ERROR_COUNT+1))
+fi
+
+if [ -z "$BOSH_CLIENT_SECRET" ]
+then
+    echo "ERROR: missing environment variable: BOSH_CLIENT_SECRET"
+    ERROR_COUNT=$(($ERROR_COUNT+1))
+fi
+
+if [ -z "$BOSH_CA_CERT" -a -z "$BOSH_CERT_FILE" ]
+then
+    echo "ERROR: missing environment variable, requires at least one of them: BOSH_CA_CERT or BOSH_CERT_FILE"
+    ERROR_COUNT=$(($ERROR_COUNT+1))
+fi
+
+
+if [ $ERROR_COUNT -ne 0 ]
+then
+    exit $ERROR_COUNT
+fi
+
+echo "targeting ${BOSH_HOST}"
+
+DIRECTOR_IP_URL_WITHOUT_PORT=${BOSH_HOST%%:25555}
+DIRECTOR_IP=$(nslookup ${DIRECTOR_IP_URL_WITHOUT_PORT##https://} 2>/dev/null|grep Address|cut -d':' -f2)
+
+DIRECTOR_IP_NO_SPACE=$(echo $DIRECTOR_IP |tr -d [:blank:])
+export BOSH_ENVIRONMENT="https://${DIRECTOR_IP_NO_SPACE}:25555"
+
+echo "Using BOSH_ENVIRONMENT=${BOSH_ENVIRONMENT}"
+bosh log-in $CA_CERT_FILE
+bosh env
