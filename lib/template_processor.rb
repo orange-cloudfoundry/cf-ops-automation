859c3d2c60b55c1c99caa6f7ac286f58a2464a05
Olivier ORAND <olivier.orand@orange.com>
2017-07-24 18:18:37 +0200

    lib(template_processor): create from generate-depls
    we move erb processing into a dedicated class. We add binding to erb, we
    have to rename BOSH_CERT to bosh_cert as it is not possible to set
    constant in binding.

---
+++ lib/template_processor.rb
@@ -0,0 +1,58 @@
+require 'yaml'
+require 'erb'
+require 'fileutils'
+
+class TemplateProcessor
+  attr_reader :root_deployment_name, :config, :context
+
+  def initialize(root_deployment_name, config = {}, context = {})
+    @root_deployment_name = root_deployment_name
+    @config = config
+    @context = context
+
+  end
+
+  def process(dir)
+    processed_template_count = 0
+
+    Dir[dir].each do |filename|
+      processed_template_count += 1
+
+      puts "processing #{filename}"
+
+      output = ERB.new(File.read(filename), 0, '<>').result(load_context_into_a_binding)
+      puts output if config[:dump_output]
+
+      # trick to avoid pipeline name like ops-depls-depls-generated or ops-depls--generated
+      tmp_pipeline_name = filename.split('/').last.chomp('-pipeline.yml.erb').chomp('depls')
+      pipeline_name = "#{@root_deployment_name}-"
+      pipeline_name << "#{tmp_pipeline_name}-" if !tmp_pipeline_name.nil? && !tmp_pipeline_name.empty?
+      pipeline_name << 'generated.yml'
+
+      puts "Pipeline name #{pipeline_name}"
+      target_dir = "#{config[:output_path]}/pipelines"
+      FileUtils.mkdir_p target_dir unless Dir.exist?(target_dir)
+      a_pipeline = File.new("#{config[:output_path]}/pipelines/#{pipeline_name}", 'w')
+      a_pipeline << output
+      puts "Trying to parse generated Yaml: #{pipeline_name} (#{a_pipeline&.path})"
+      YAML.load_file(a_pipeline)
+      puts "> #{pipeline_name} seems a valid Yaml file"
+      puts '####################################################################################'
+      puts '####################################################################################'
+    end
+
+    processed_template_count
+  end
+
+  private
+
+  def load_context_into_a_binding
+    new_binding = binding
+    context.each do |k, v|
+      new_binding.local_variable_set k.to_sym, v
+    end
+    puts "Local var: #{new_binding.local_variables}"
+    new_binding
+  end
+
+end
