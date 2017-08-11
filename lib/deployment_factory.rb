1ad111317238f3415774c624e4a82977a9565ab3
Olivier ORAND <olivier.orand@orange.com>
2017-08-11 15:43:53 +0200

    lib(deployment_factory): disable version format check
    these checks should be part of a dedicated class

--- lib/deployment_factory.rb
+++ lib/deployment_factory.rb
@@ -1,12 +1,7 @@
 require_relative 'deployment'
 
 class DeploymentFactory
-  attr_reader :version_reference
-
-
-  DEPLOYMENT_NAME='deployment-name'
-  STEMCELL_VERSION='stemcell-version'
-  STEMCELL_NAME='stemcell-name'
+  attr_reader :version_reference, :root_deployment_name
 
   def initialize(root_deployment_name, version_reference = {})
     @version_reference = version_reference
@@ -15,38 +10,46 @@ class DeploymentFactory
   end
 
   def load_file(filename)
-    current_dependencies = YAML.load_file(filename)
-    load(current_dependencies)
+    raise "file not found: #{filename}" unless File.exist?(filename)
+    load(YAML.load_file(filename))
   end
 
   def load(data = {})
     deployments = []
     data['deployment'].each do |deployment_name, deployment_details|
-      boshrelease_list = deployment_details['releases']
-      boshrelease_list&.each do |a_release, _|
-        version = version_reference[a_release + '-version']
-        deployment_details['releases'][a_release]['version'] = version
-      end
-      deployment_details['stemcells'].each do |a_stemcell, _|
-        raise "Invalid stemcell: expected <#{@version_reference['stemcells-name']}> - Found <#{a_stemcell}>" if a_stemcell != @version_reference['stemcell-name']
-
-        version = @version_reference['stemcell-version']
-
-        deployments << Deployment.new(deployment_name, deployment_details)
-      end
+      update_deployment_version!(deployment_details)
+      deployments << Deployment.new(deployment_name, deployment_details)
     end
     raise "Invalid data. Missing root: 'deployment'" if deployments.empty?
     deployments
   end
 
-  private
-
-  def validate
-    raise "invalid/missing #{DEPLOYMENT_NAME}" if @root_deployment_name.nil? || @root_deployment_name.empty?
-    raise "invalid/missing #{DEPLOYMENT_NAME}" unless @version_reference[DEPLOYMENT_NAME] = @root_deployment_name
-    raise "invalid/missing #{STEMCELL_VERSION}" if @version_reference[STEMCELL_VERSION].nil? #|| @version_reference[STEMCELL_VERSION].empty?
-    raise "invalid/missing #{STEMCELL_NAME}" if @version_reference[STEMCELL_NAME].nil? || @version_reference[STEMCELL_NAME].empty?
+  def update_deployment_version!(deployment_details)
+    update_boshrelease_version!(deployment_details)
+    update_stemcell_version!(deployment_details)
   end
 
+
+  private
+
+  def update_stemcell_version!(deployment_details)
+    deployment_details['stemcells'].each do |a_stemcell, _|
+      raise "Invalid stemcell: expected <#{@version_reference['stemcells-name']}> - Found <#{a_stemcell}>" if a_stemcell != @version_reference['stemcell-name']
+      version = @version_reference['stemcell-version']
+    end
+  end
+
+  def update_boshrelease_version!(deployment_details)
+    boshrelease_list = deployment_details['releases']
+    boshrelease_list&.each do |a_release, _|
+      version = version_reference[a_release + '-version']
+      raise "Missing boshrelease version: expecting '#{a_release}-version' key in #{@root_deployment_name}-versions.yml" if version.nil?
+      deployment_details['releases'][a_release]['version'] = version
+    end
+  end
+
+  def validate
+    raise 'invalid/missing root_deployment_name' if @root_deployment_name.nil? || @root_deployment_name.empty?
+  end
 
 end
