bfc11cc93c245e76a06add25026b30080c949ae2
Olivier ORAND <olivier.orand@orange.com>
2017-11-13 15:58:36 +0100

    pipeline(depls): move stemcell support from bosh.io to S3
    Check-resource is also impacted as S3 resource requires a full scan path.
    To avoid conflict with previous 'stemcell-name' properties, we use a new
    property 'stemcell-main-name'. To get the full stemcell name, a concatenation of stemcell-name-prefix and stemcell-main-name is required.
    
    Requires new concourse properties
        stemcell-name-prefix
        stemcell-main-name
        s3-stemcell-bucket
        s3-stemcell-region-name
        s3-stemcell-access-key-id
        s3-stemcell-secret-key
        s3-stemcell-endpoint
        s3-stemcell-skip-ssl-verification

--- lib/secrets.rb
+++ lib/secrets.rb
@@ -11,11 +11,11 @@ class Secrets
 
     Dir[@secrets_root_dir].select { |f| File.directory? f }.each do |depls_level_dir|
       depls_level_name = depls_level_dir.split('/').last
-      puts "Processing depls level: #{depls_level_name}"
+      puts "Processing Secrets depls level: #{depls_level_name}"
       dir_overview[depls_level_name] = []
       Dir[depls_level_dir + '/*'].select { |f| File.directory? f }.each do |boshrelease_level_dir|
         boshrelease_level_name= boshrelease_level_dir.split('/').last
-        puts "Processing boshrelease level: #{depls_level_name} -- #{boshrelease_level_name}"
+        puts "Processing Secrets boshrelease level: #{depls_level_name} -- #{boshrelease_level_name}"
         dir_overview[depls_level_name] << boshrelease_level_name
       end
     end
