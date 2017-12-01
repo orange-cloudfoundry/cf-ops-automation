36cf002a08be461f5d3fd415ea82ab05b28ac6f4
Olivier ORAND <olivier.orand@orange.com>
2017-12-01 17:33:16 +0100

    task(bosh_update_*_config): selects and use vars and ops file to update config
    We expect manifest, operator files and vars files to be present in
    config-manifest directory. So we don't have to handle iaas specifics in
    these tasks. It should be done by generate_manifest task.

---
+++ concourse/tasks/bosh_update_runtime_config/run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2015-2017 Orange
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+set -eC
+
+ls -lrt config-manifest
+
+VARS_FILES=""
+OPS_FILES=""
+
+for a_vars_file in $(ls ./config-manifest/*${VARS_FILES_SUFFIX}); do
+    VARS_FILES="-l ${a_vars_file} ${VARS_FILES}"
+done
+
+for an_ops_file in $(ls ./config-manifest/*${OPS_FILES_SUFFIX}); do
+    OPS_FILES="-o ${an_ops_file} ${OPS_FILES}"
+done
+
+echo "Operators detected: <${OPS_FILES}>"
+echo "Vars files detected: <${VARS_FILES}>"
+
+source ./script-resource/scripts/bosh_cli_v2_login.sh ${BOSH_TARGET}
+cat config-manifest/runtime-config.yml
+OLD_CONFIG=$(mktemp runtime-config-XXXXXX)
+bosh runtime-config >$OLD_CONFIG || true
+diff $OLD_CONFIG config-manifest/runtime-config.yml || true
+
+bosh -n int ${VARS_FILES} ${OPS_FILES} config-manifest/runtime-config.yml
+
+bosh -n update-runtime-config ${VARS_FILES} ${OPS_FILES} config-manifest/runtime-config.yml
+
