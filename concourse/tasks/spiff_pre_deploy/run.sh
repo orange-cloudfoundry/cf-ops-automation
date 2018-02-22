75485777e37de27cf37306e1cdcf5231ad035e3c
Olivier ORAND <olivier.orand@orange.com>
2018-02-22 10:03:21 +0100

    task(spiff_pre_deploy,post_deploy): fixes missing additional resource
    We always need to copy additional resource to output_dir, not only when a
    hook script is found !

--- concourse/tasks/spiff_pre_deploy/run.sh
+++ concourse/tasks/spiff_pre_deploy/run.sh
@@ -6,17 +6,16 @@ CURRENT_DIR=$(pwd)
 OUTPUT_DIR=${OUTPUT_DIR:-${CURRENT_DIR}/generated-files}
 COMMON_SCRIPT_DIR=${COMMON_SCRIPT_DIR:-scripts-resource/scripts/manifest}
 
+cp -r additional-resource/. ${OUTPUT_DIR}/
 
 if [ -n "$CUSTOM_SCRIPT_DIR" ]; then
     if [ -f "$CUSTOM_SCRIPT_DIR/pre-deploy.sh" ]; then
         echo "pre deploy script detected"
-        cp -r additional-resource/. ${OUTPUT_DIR}/
         chmod +x $CUSTOM_SCRIPT_DIR/pre-deploy.sh
         GENERATE_DIR=${OUTPUT_DIR} BASE_TEMPLATE_DIR=${CUSTOM_SCRIPT_DIR} ${CUSTOM_SCRIPT_DIR}/pre-deploy.sh
     elif [ -f "$CUSTOM_SCRIPT_DIR/pre-bosh-deploy.sh" ]; then
         echo "**deprecated** LEGACY pre bosh deploy script detected"
         echo "Please rename pre-bosh-deploy.sh to pre-deploy.sh to be compliant"
-        cp -r additional-resource/. ${OUTPUT_DIR}/
         chmod +x $CUSTOM_SCRIPT_DIR/pre-bosh-deploy.sh
         GENERATE_DIR=${OUTPUT_DIR} BASE_TEMPLATE_DIR=${CUSTOM_SCRIPT_DIR} ${CUSTOM_SCRIPT_DIR}/pre-bosh-deploy.sh
     else
--- spec/tasks/spiff_pre_deploy/task_spec.rb
+++ spec/tasks/spiff_pre_deploy/task_spec.rb
@@ -37,7 +37,10 @@ describe 'spiff_pre_deploy task' do
 
     it 'displays an ignore message' do
       expect(@output).to include('ignoring pre-deploy. No my-custom-script-dir/pre-deploy.sh or pre-bosh-deploy.sh detected')
+    end
 
+    it 'adds additional resource to generated' do
+      expect(File).to exist(File.join(generated_files, 'my_additional_file.yml'))
     end
   end
 
