9d8efa31c4beb407f5be3b8361e6b0aa7aec9db5
Olivier ORAND <olivier.orand@orange.com>
2018-03-08 17:09:34 +0100

    refactor: fix minor rubocop and reek violations

--- concourse/tasks/list_used_ci_team/run.rb
+++ concourse/tasks/list_used_ci_team/run.rb
@@ -5,7 +5,7 @@ task_root_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '
 require "#{task_root_dir}/cf-ops-automation/lib/ci_deployment_overview"
 ci_overview = CiDeploymentOverview.new(File.join(ENV.fetch('SECRETS_PATH', 'secrets'), '*-depls')).overview
 
-result = CiDeploymentOverview.getTeams(ci_overview)
+result = CiDeploymentOverview.teams(ci_overview)
 
 output_dir = 'ci-deployment-overview'.freeze
 
