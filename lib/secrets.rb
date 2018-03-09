b843baae2c0a616af0b3fd5f34e0650fd347c4c9
Olivier ORAND <olivier.orand@orange.com>
2018-03-09 13:27:04 +0100

    lib(secrets): refactor minor rubocop and reek violations

--- lib/secrets.rb
+++ lib/secrets.rb
@@ -9,18 +9,25 @@ class Secrets
   def overview
     dir_overview = {}
 
-    Dir[@secrets_root_dir].select { |f| File.directory? f }.each do |depls_level_dir|
+    Dir[@secrets_root_dir].select { |file_or_dir| File.directory? file_or_dir }
+                          .each do |depls_level_dir|
       depls_level_name = depls_level_dir.split('/').last
       puts "Processing Secrets depls level: #{depls_level_name}"
-      dir_overview[depls_level_name] = []
-      Dir[depls_level_dir + '/*'].select { |f| File.directory? f }.each do |boshrelease_level_dir|
-        boshrelease_level_name= boshrelease_level_dir.split('/').last
-        puts "Processing Secrets boshrelease level: #{depls_level_name} -- #{boshrelease_level_name}"
-        dir_overview[depls_level_name] << boshrelease_level_name
-      end
+      dir_overview[depls_level_name] = subdir_overview(depls_level_dir, depls_level_name)
     end
     dir_overview
   end
 
+  private
 
+  def subdir_overview(depls_level_dir, depls_level_name)
+    overview = []
+    Dir[depls_level_dir + '/*'].select { |file_or_dir| File.directory? file_or_dir }
+                               .each do |boshrelease_level_dir|
+      boshrelease_level_name = boshrelease_level_dir.split('/').last
+      puts "Processing Secrets boshrelease level: #{depls_level_name} -- #{boshrelease_level_name}"
+      overview << boshrelease_level_name
+    end
+    overview
+  end
 end
