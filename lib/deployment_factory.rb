755079dc69314de5d7f4ebdbf63e6e25a7b7c8b2
Olivier ORAND <olivier.orand@orange.com>
2018-05-15 18:17:33 +0200

    lib(deployment_factory): add config object to retrieve stemcell name
    we get stemcell name from config instead of version_reference
    
    part of #129

--- lib/deployment_factory.rb
+++ lib/deployment_factory.rb
@@ -1,17 +1,21 @@
 require_relative 'deployment'
+require_relative 'config'
 
 class DeploymentFactory
-  attr_reader :version_reference, :root_deployment_name
+  attr_reader :version_reference, :root_deployment_name, :config
 
-
-  def initialize(root_deployment_name, version_reference = {})
+  def initialize(root_deployment_name, version_reference = {}, config = Config.new)
     @version_reference = {}
     @version_reference = version_reference unless version_reference.nil?
+    @config = config
     @root_deployment_name = root_deployment_name
     validate
+    validate_config
+    validate_version_reference
   end
 
   def load_file(filename)
+    raise 'invalid filename. Cannot be nil' if filename.nil?
     raise "file not found: #{filename}" unless File.exist?(filename)
     puts "processing #{filename}"
     yaml_file = YAML.load_file(filename) || {}
@@ -19,32 +23,38 @@ class DeploymentFactory
   end
 
   def load(data = {})
+    raise "invalid data. Cannot load 'nil' data" if data.nil?
     deployments = []
     data['deployment']&.each do |deployment_name, deployment_details|
-      update_deployment_version!(deployment_details)
+      update_deployment_details(deployment_details)
       deployments << Deployment.new(deployment_name, deployment_details)
     end
     raise "Invalid data. Missing root: 'deployment' or '<deployment_name>'" if deployments.empty?
     deployments
   end
 
-  def update_deployment_version!(deployment_details)
-    update_boshrelease_version!(deployment_details)
-    update_stemcell_version!(deployment_details)
+  def update_deployment_details(deployment_details)
+    update_boshrelease_version(deployment_details)
+    add_stemcell_info(deployment_details)
   end
 
+  def stemcell_name
+    @config.stemcell_name
+  end
+
+  def stemcell_version
+    @version_reference['stemcell-version']
+  end
 
   private
 
-  def update_stemcell_version!(deployment_details)
+  def add_stemcell_info(deployment_details)
     return if deployment_details.nil?
-    deployment_details['stemcells']&.each do |a_stemcell, _|
-      raise "Invalid stemcell: expected <#{@version_reference['stemcells-name'] || 'version reference not empty'}> - Found <#{a_stemcell}>" if a_stemcell != @version_reference['stemcell-name']
-      version = @version_reference['stemcell-version']
-    end
+    deployment_details['stemcells'] = { stemcell_name => {} }
+    # deployment_details['stemcells']
   end
 
-  def update_boshrelease_version!(deployment_details)
+  def update_boshrelease_version(deployment_details)
     return if deployment_details.nil?
     boshrelease_list = deployment_details['releases']
     boshrelease_list&.each do |a_release, _|
@@ -58,4 +68,12 @@ class DeploymentFactory
     raise 'invalid/missing root_deployment_name' if @root_deployment_name.nil? || @root_deployment_name.empty?
   end
 
+  def validate_config
+    raise 'invalid config: cannot be nil' if @config.nil?
+    raise 'invalid config: missing stemcell, expected: a config with a stemcell name defined' if @config.stemcell_name.to_s.empty?
+  end
+
+  def validate_version_reference
+    raise 'invalid version: missing stemcell version' if @version_reference['stemcell-version'].to_s.empty?
+  end
 end
