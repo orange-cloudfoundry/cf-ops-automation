81305fca73856c49377ec3c2ab5154bf49c0b94a
Olivier ORAND <olivier.orand@orange.com>
2018-05-15 18:17:33 +0200

    lib(deployment_factory): add config object to retrieve stemcell name
    we get stemcell name from config instead of version_reference
    
    part of #129

--- spec/lib/deployment_factory_spec.rb
+++ spec/lib/deployment_factory_spec.rb
@@ -4,47 +4,136 @@ require_relative '../../lib/deployment_factory'
 describe DeploymentFactory do
   let(:root_deployment_name) { 'main_depls' }
   let(:deployment_name) { 'my_deployment' }
+  let(:config) { Config.new }
+  let(:versions) do
+    { 'deployment-name' => root_deployment_name, 'stemcell-version' => '10.0',
+      'bosh-version' => '264.10.0',
+      'bosh-openstack-cpi-release-version' => '37' }
+  end
 
   describe '#initialize' do
-    it 'validates param'
+    subject { described_class.new(root_deployment_name, versions, config) }
+
+    context 'when version is valid' do
+      it 'contains a stemcell version' do
+        expect(subject.version_reference).to include('stemcell-version')
+      end
+    end
 
     context 'when version is invalid' do
-      it 'deployment-name'
-      it 'stemcell-version'
-      it 'stemcell-name'
+      let(:versions) {}
+
+      it 'raise an error about stemcell-version' do
+        expect { subject }.to raise_error(RuntimeError) { |error| expect(error.message).to match('invalid version: missing stemcell version') }
+      end
+    end
+
+    context 'when config is valid' do
+      it 'contains a stemcell name' do
+        expect(subject.stemcell_name).to eq(Config::DEFAULT_STEMCELL)
+      end
+    end
+
+    context 'when config is nil' do
+      let(:config) {}
+
+      it 'complains about nil config' do
+        expect { subject }.to raise_error(RuntimeError) { |error| expect(error.message).to match('invalid config: cannot be nil') }
+      end
+    end
+
+    context 'when config is incomplete' do
+      let(:config) { instance_double(Config) }
+
+      it 'raises an error about stemcell-name' do
+        allow(config).to receive(:stemcell_name).and_return('')
+
+        expect { subject }.to raise_error(RuntimeError) { |error| expect(error.message).to match('invalid config: missing stemcell, expected: a config with a stemcell name defined') }
+      end
     end
   end
 
   describe '#load_file' do
     context 'when file does not exist' do
-      it 'raise an error'
+      subject { described_class.new(root_deployment_name, versions, config).load_file 'dummy-filename.yml' }
+
+      it 'raise an error' do
+        expect { subject }.to raise_error(RuntimeError) { |error| expect(error.message).to match('file not found: dummy-filename.yml') }
+
+      end
     end
 
     context 'when filename is nil ' do
-      it 'raise an error'
+      subject { described_class.new(root_deployment_name, versions, config).load_file nil }
+
+      it 'raise an error' do
+        expect { subject }.to raise_error(RuntimeError) { |error| expect(error.message).to match('invalid filename. Cannot be nil') }
+
+      end
     end
 
     context 'when filename is empty ' do
-      it 'raise an error'
+      subject { described_class.new(root_deployment_name, versions, config).load_file '' }
+
+      it 'raise an error' do
+        expect { subject }.to raise_error(RuntimeError) { |error| expect(error.message).to match('file not found: ') }
+
+      end
     end
   end
 
   describe '#load' do
     context 'when data is nil ' do
-      it 'raise an error'
+      let(:deployment_factory) do
+        described_class.new(
+            root_deployment_name,
+            versions
+        )
+      end
+
+      it 'raise an error' do
+        expect { deployment_factory.load(nil) }.to raise_error(RuntimeError) { |error| expect(error.message).to match("invalid data. Cannot load 'nil' data") }
+      end
     end
 
     context 'when a deployment does not have any details' do
       let(:deployment_factory) do
         described_class.new(
           root_deployment_name,
-          nil
+          versions
         )
       end
       let(:loaded_deployments) { deployment_factory.load('deployment' => { deployment_name => nil }) }
 
       it 'creates a deployment object with an empty details field' do
         expect(loaded_deployments.first).to have_attributes(name: deployment_name, details: {})
+      end
+    end
+
+    context 'when a deployment is loaded' do
+      let(:versions) do
+        { 'deployment-name' => root_deployment_name, 'stemcell-version' => '10.0',
+          'bosh-version' => '264.10.0',
+          'bosh-openstack-cpi-release-version' => '37' }
+      end
+      let(:deployment_factory) { described_class.new(root_deployment_name, versions, config) }
+      let(:loaded_deployments) { deployment_factory.load('deployment' => bosh_master_deployment) }
+      let(:bosh_master_deployment) do
+        my_yaml = <<~YAML
+          #{deployment_name}:
+            releases:
+              bosh:
+                base_location: https://bosh.io/d/github.com/
+                repository: cloudfoundry/bosh
+              bosh-openstack-cpi-release:
+                base_location: https://bosh.io/d/github.com/
+                repository: cloudfoundry-incubator/bosh-openstack-cpi-release
+        YAML
+        YAML.safe_load(my_yaml)
+      end
+
+      it 'creates an enhanced deployment' do
+        expect(loaded_deployments.first).to have_attributes(name: deployment_name, details: include('stemcells' => { 'bosh-openstack-kvm-ubuntu-trusty-go_agent' => {} }))
       end
     end
   end
