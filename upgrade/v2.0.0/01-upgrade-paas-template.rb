9ddf9ab314d5a4aebc2885879199f0aae627e1dc
Olivier ORAND <olivier.orand@orange.com>
2018-05-16 11:44:49 +0200

    script(generate-depls): remove stemcell-name from config file
    we also use config to create deployment_factory and provides a migration
    scripts to remove stemcells key from paas-template repository
    
    closes #129

---
+++ upgrade/v2.0.0/01-upgrade-paas-template.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+require 'yaml'
+require 'optparse'
+require_relative '../../lib/deployment_deployers_config'
+
+# Argument parsing
+options = {
+  ops_automation: '.',
+  dump_output: true,
+  paas_template_root: '../paas-templates'
+}
+
+opt_parser = OptionParser.new do |opts|
+  opts.banner = "Incomplete/wrong parameter(s): #{opts.default_argv}.\n Usage: ./#{opts.program_name} <options>"
+
+  opts.on('-t', '--templates-path PATH', "paas-templates location (main git directory). Default: #{options[:paas_template_root]}") do |tp_string|
+    options[:paas_template_root] = tp_string
+  end
+
+  opts.on('-a', '--automation-path PATH', 'Base location for cf-ops-automation') do |ap_string|
+    options[:ops_automation] = ap_string
+  end
+
+  opts.on('--[no-]dump', 'Dump genereted file on standart output') do |dump|
+    options[:dump_output] = dump
+  end
+end
+opt_parser.parse!
+
+paas_template_root = options[:paas_template_root]
+
+raise "invalid paas_template_root: <#{paas_template_root}> does not exist" unless Dir.exist?(paas_template_root)
+
+def delete_stemcells_key(deployment_dependencies)
+  migrated = false
+  deployment_dependencies['deployment']&.each do |_, details|
+    details&.delete('stemcells') && migrated = true
+  end
+  migrated
+end
+
+def remove_stemcell_key_from_deployment_dependencies(deployment_dependencies_files)
+  migrated_counter = 0
+  deployment_dependencies_files&.each do |deployment_dependencies_filename|
+    deployment_dependencies = YAML.load_file(deployment_dependencies_filename)
+    unless deployment_dependencies
+      puts "*** ignoring file: #{deployment_dependencies_filename} - invalid format"
+      next
+    end
+    puts "original #{deployment_dependencies_filename}: #{deployment_dependencies}"
+
+    if delete_stemcells_key(deployment_dependencies)
+      migrated_counter += 1
+      puts "migrated #{deployment_dependencies_filename}: #{deployment_dependencies}"
+    end
+    File.open(deployment_dependencies_filename, 'w') { |file| file.write deployment_dependencies.to_yaml }
+    puts '#########'
+  end
+  migrated_counter
+end
+
+puts "scanning for deployment-dependencies in #{paas_template_root}"
+
+deployment_dependencies_files = Dir["#{paas_template_root}/**/deployment-dependencies.yml"]
+
+migrated_deployment_dependencies_counter = remove_stemcell_key_from_deployment_dependencies(deployment_dependencies_files)
+
+puts
+puts 'Summary:'
+puts "  #{migrated_deployment_dependencies_counter}/#{deployment_dependencies_files.length} deployment-dependencies.yml migrated"
+puts
+
+puts
+puts 'Thanks, Orange CloudFoundry SKC'
