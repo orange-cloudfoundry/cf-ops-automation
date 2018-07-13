cb6ff816f7c92bde005eb8a20bc038da8a56852b
Olivier ORAND <olivier.orand@orange.com>
2018-07-13 16:26:38 +0200

    pipeline(depls): online stemcells support
    By default, we use offline stemcell, but it can be overridden by `shared-config.yml` or `private-config.yml`. We also rename `bosh-stemcell` resource to `((stemcell-main-name))` to be clearer.
    
    closes #128

--- scripts/concourse-manual-pipelines-update.rb
+++ scripts/concourse-manual-pipelines-update.rb
@@ -1,37 +1,14 @@
 #!/usr/bin/env ruby
-# encoding: utf-8
-# set -e
-
-#echo "Deploy on ${FLY_TARGET} using secrets in $SECRET_DIR"
-#for depls in ${DEPLS_LIST};do
-#    cd ${SCRIPT_DIR}/concourse
-#    ./generate-depls.rb -d ${depls} -p ${SECRET_DIR} -o ${OUTPUT_DIR} --no-dump
-#    PIPELINE="${depls}-init-generated"
-#    cd ${SCRIPT_DIR}
-#    echo "Load ${PIPELINE} on ${FLY_TARGET}"
-#    set +e
-#    fly -t ${FLY_TARGET} set-pipeline -p ${PIPELINE} -c ${OUTPUT_DIR}/pipelines/${PIPELINE}.yml  \
-#                -l ${SECRET_DIR}/micro-depls/concourse-micro/pipelines/credentials-auto-init.yml \
-#                -l ${SECRET_DIR}/micro-depls/concourse-micro/pipelines/credentials-mattermost-certs.yml \
-#                -l ${SECRET_DIR}/micro-depls/concourse-micro/pipelines/credentials-git-config.yml
-#    set -e
-#    fly -t ${FLY_TARGET} unpause-pipeline -p ${PIPELINE}
-#    if [ "$SKIP_TRIGGER" != "true" ]
-#    then
-#        fly -t ${FLY_TARGET} trigger-job -j "${PIPELINE}/update-pipeline-${depls}"
-#    fi
-#done
-
 
 require 'yaml'
 require 'optparse'
 
 # Argument parsing
 OPTIONS = {
-  :depls => 'ops-depls',
-  :no_interactive => false,
-  :fail_fast => false,
-  :fail_on_error => false
+  depls: 'ops-depls',
+  no_interactive: false,
+  fail_fast: false,
+  fail_on_error: false
 }
 opt_parser = OptionParser.new do |opts|
   opts.banner = 'Usage: ./scripts/concourse-manual-pipelines-update.sh [options]
@@ -40,8 +17,7 @@ Customization using ENVIRONMENT_VARIABLE:
     PAAS_TEMPLATES: paas-templates to use - Default: ../paas-templates
     DEBUG: enable debug message - Default: false
     PIPELINES_DIR: pipelines ready to be uploaded directory- Default: boostrap-generated/pipelines
-    TARGET_NAME - Default: cw-pp-micro
-
+    TARGET_NAME - Default: fe-int
 '
 
   opts.on("--without=WITHOUT", "-wWITHOUT", "Don't update matched pipelines") do |without_string|
@@ -71,20 +47,15 @@ Customization using ENVIRONMENT_VARIABLE:
   opts.on('--fail-on-error', 'Fail on loading error') do |_|
     OPTIONS[:fail_on_error] = true
   end
-
 end
 opt_parser.parse!
 
-SECRETS = ENV['SECRETS'] ||"../preprod-secrets"
-PAAS_TEMPLATES = ENV['PAAS_TEMPLATES'] ||'../paas-templates'
+SECRETS = ENV['SECRETS'] || "../preprod-secrets"
+PAAS_TEMPLATES = ENV['PAAS_TEMPLATES'] || '../paas-templates'
 DEBUG = ENV['DEBUG'] || false
 PIPELINES_DIR = ENV['PIPELINES_DIR'] || 'bootstrap-generated/pipelines'
 
-
-flyrc = YAML.load_file(File.expand_path('~/.flyrc'))
-target_name = ENV['TARGET_NAME'] || 'cw-pp-micro'
-target = flyrc['targets'][target_name]
-concourse_url= target['api']
+target_name = ENV['TARGET_NAME'] || 'fe-int'
 
 PIPELINE_PREFIX = ENV['PIPELINE_PREFIX'] || ''
 
@@ -98,20 +69,20 @@ def get_pipeline_name(name)
 end
 
 def set_pipeline(target_name:, name:, config:, load: [], options: [])
-  return if OPTIONS.has_key?(:match) && !name.include?(OPTIONS[:match])
-  return if OPTIONS.has_key?(:without) && name.include?(OPTIONS[:without])
-  puts "   #{name} pipeline"
+  return if OPTIONS.key?(:match) && !name.include?(OPTIONS[:match])
+  return if OPTIONS.key?(:without) && name.include?(OPTIONS[:without])
+  puts "   Setting #{name} pipeline"
 
-  fly_cmd=(%{bash -c "fly -t #{target_name} set-pipeline \
+  fly_cmd = %{bash -c "fly -t #{target_name} set-pipeline \
     -p #{get_pipeline_name(name)} \
     -c #{config} \
   #{load.collect { |l| "-l #{l}" }.join(' ')} \
-  #{options.collect { |opt| "#{opt}" }.join(' ')}
-  "})
+  #{options.collect(&:to_s).join(' ')}
+  "}
 
   puts "Executing: #{fly_cmd}"
 
-  pipeline_successfully_loaded=system(fly_cmd)
+  pipeline_successfully_loaded = system(fly_cmd)
   puts "Pipeline successfully loaded: #{pipeline_successfully_loaded}"
   if OPTIONS[:fail_fast] && !pipeline_successfully_loaded
     raise "Failed to load pipeline #{get_pipeline_name(name)} from template #{name}"
@@ -122,11 +93,11 @@ end
 def generate_full_path_for_concourse_vars_files(vars_files)
   vars_files_with_path = []
   vars_files.each do |var_file|
-    if var_file =~ /versions.yml/
-      vars_files_with_path << "#{PAAS_TEMPLATES}/#{var_file}"
-    else
-      vars_files_with_path << "#{SECRETS}/#{var_file}"
-    end
+    vars_files_with_path << if var_file =~ /versions.yml/
+                              "#{PAAS_TEMPLATES}/#{var_file}"
+                            else
+                              "#{SECRETS}/#{var_file}"
+                            end
   end
   vars_files_with_path
 end
@@ -139,11 +110,11 @@ def update_pipelines(target_name)
     # puts "Found #{filename}"
     next if OPTIONS.key?(:depls) && !filename.include?(OPTIONS[:depls])
     next if OPTIONS.key?(:template) && !filename.include?(OPTIONS[:template])
-    puts "Processing only #{filename}"
+    puts "Starting processing of #{filename}"
     deployment_name = File.basename(filename, '.yml')
     ci_deployment_overview = YAML.load_file("#{SECRETS}/#{depls}/ci-deployment-overview.yml")
 
-    pipelines=ci_deployment_overview['ci-deployment'][depls]['pipelines']
+    pipelines = ci_deployment_overview['ci-deployment'][depls]['pipelines']
     current_pipeline = pipelines[deployment_name]
     if current_pipeline.nil?
       puts "invalid config #{SECRETS}/#{depls}/ci-deployment-overview.yml should contains a key ...[pipelines][#{deployment_name}][vars_files]"
@@ -164,11 +135,10 @@ def update_pipelines(target_name)
     )
     loaded_pipelines_status[deployment_name] = result
   end
+
   if OPTIONS[:fail_on_error]
-    raise "pipeline loading error. Summary #{loaded_pipelines_status}" unless loaded_pipelines_status.select {|name, status| !status.nil? && !status}.empty?
+    raise "pipeline loading error. Summary #{loaded_pipelines_status}" unless loaded_pipelines_status.select { |_, status| !status.nil? && !status }.empty?
   end
 end
 
-
 update_pipelines target_name
-
