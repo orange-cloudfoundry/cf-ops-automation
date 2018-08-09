fe5c7b3645a2ae5e71f4e42470ec4aa7e1831422
Olivier ORAND <olivier.orand@orange.com>
2018-08-09 12:02:06 +0200

    upgrade(3.0.0): enhance user message

--- upgrade/v3.0.0/01-upgrade-config.rb
+++ upgrade/v3.0.0/01-upgrade-config.rb
@@ -14,7 +14,9 @@ def insert_iaas_specific_pipeline(ci_deployment_overview)
   ci_deployment_overview['ci-deployment']&.each do |_, details|
     details['pipelines']&.each do |name, pipeline_config|
       if name.end_with?('-init-generated')
-        unless pipeline_config['vars_files'].include?('micro-depls/concourse-micro/pipelines/credentials-iaas-specific.yml')
+        if pipeline_config['vars_files'].include?('micro-depls/concourse-micro/pipelines/credentials-iaas-specific.yml')
+          puts "\t>> INFO: required credentials already part of #{name} configuration"
+        else
           pipeline_config['vars_files'] << 'micro-depls/concourse-micro/pipelines/credentials-iaas-specific.yml'
           migrated = true
         end
@@ -34,12 +36,14 @@ def insert_iaas_specific_into_init_pipeline_config(ci_deployment_overview_files)
     end
     # puts "original #{ci_deployment_filename}: #{deployment_dependencies}"
 
+    puts "Analyzing #{ci_deployment_filename}"
     if insert_iaas_specific_pipeline(ci_deployment)
       migrated_counter += 1
-      puts "migrated #{ci_deployment_filename}"
+      puts "\t>> INFO: migrated #{ci_deployment_filename}"
+    else
+      puts "\t>> INFO: ignored #{ci_deployment_filename}"
     end
     File.open(ci_deployment_filename, 'w') { |file| file.write ci_deployment.to_yaml }
-    puts '#########'
   end
   migrated_counter
 end
