649a33eb5d7b8e851c7a5da99fab849e9a457225
Olivier ORAND <olivier.orand@orange.com>
2018-09-18 16:51:27 +0200

    task(execute_deploy_script): add execution tests
    we ensure the image is executable

--- spec/tasks/execute_deploy_script/task_spec.rb
+++ spec/tasks/execute_deploy_script/task_spec.rb
@@ -5,6 +5,47 @@ require_relative '../task_spec_helper'
 
 describe 'execute_deploy_script task' do
 
+  context 'when no bosh is available' do
+
+    before(:context) do
+      @templates_dir =  Dir.mktmpdir
+      @secrets_dir = Dir.mktmpdir
+      @result_dir = Dir.mktmpdir
+
+      fly_cli_environment = {
+          'BOSH_TARGET' => 'https://dummy-bosh',
+          'BOSH_CLIENT' => 'aUser',
+          'BOSH_CLIENT_SECRET' => 'aPassword',
+          'BOSH_CA_CERT' => 'dummy-cert',
+          'CURRENT_DEPLS' => 'xxx',
+          'COMMON_SCRIPT_DIR' => 'yyy',
+          'SECRETS_DIR' => 'zzz'
+      }
+
+      @output = execute('-c concourse/tasks/execute_deploy_script.yml ' \
+        '-i scripts-resource=. ' \
+        "-i templates=#{@templates_dir} " \
+        "-i secrets=#{@secrets_dir} " \
+        "-o run-resource=#{@result_dir} ",\
+        fly_cli_environment )
+
+    end
+
+    after(:context) do
+      FileUtils.rm_rf @templates_dir
+      FileUtils.rm_rf @secrets_dir
+      FileUtils.rm_rf @result_dir
+    end
+
+    it 'tries to login' do
+      expect(@output).to include('targeting https://dummy-bosh')
+    end
+
+    it 'displays an error message' do
+      expect(@output).to include("no address for dummy-bosh")
+    end
+  end
+
   context 'when pre-requisites are valid' do
     let(:task) { YAML.load_file 'concourse/tasks/execute_deploy_script.yml' }
 
