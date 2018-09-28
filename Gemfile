44715766d71d85be6e6f4c1c2858cd37e83b43aa
Olivier ORAND <olivier.orand@orange.com>
2018-09-28 16:25:53 +0200

    task(git_update_a_dir_from_generated): create task
    we add a task to update a directory in git

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rhcl', '>= 0.1.0'
 
 group :development do
   gem 'docker_registry2', '>= 1.4.0'
+  gem 'git', '1.5.0'
   gem 'github_changelog_generator', '>= 1.14.3'
   gem 'mdl', '0.4.0'
   gem 'rake', '12.2.1'
---
+++ spec/tasks/git_update_a_dir_from_generated/task_spec.rb
@@ -0,0 +1,54 @@
+# encoding: utf-8
+
+# require 'spec_helper.rb'
+require 'yaml'
+require 'tmpdir'
+require 'fileutils'
+require 'git'
+
+describe 'git_reset_wip task' do
+
+  context 'when executed' do
+    after(:context) do
+      FileUtils.rm_rf @generated_resource
+      FileUtils.rm_rf @updated_git_resource
+      FileUtils.rm_rf @reference_resource
+    end
+
+    before(:context) do
+      @reference_resource = Dir.mktmpdir
+      @updated_git_resource = Dir.mktmpdir
+      @generated_resource = Dir.mktmpdir
+      File.open(File.join(@generated_resource,'unchanged_file.txt'), 'w') { |file| file.write('Unchanged') }
+      File.open(File.join(@generated_resource,'new_file.txt'), 'w') { |file| file.write('New') }
+
+      FileUtils.cp_r(File.join(File.dirname(__FILE__), 'reference-resource', '.'), @reference_resource)
+      repo = Git.init(@reference_resource)
+      repo.add
+      repo.commit('initialize repository')
+      @output = execute('--include-ignored -c concourse/tasks/git_update_a_dir_from_generated.yml ' \
+        "-i reference-resource=#{@reference_resource} " \
+        "-i generated-resource=#{@generated_resource} " \
+        "-o updated-git-resource=#{@updated_git_resource} ",
+                        'COMMIT_MESSAGE' => "'my commit message'",
+                        'OLD_DIR' => 'coa/pipelines/deployed/')
+    end
+
+    it 'updates files in output dir' do
+      expected_files = [File.join(@updated_git_resource,"coa/pipelines/deployed/unchanged_file.txt"), File.join(@updated_git_resource,"coa/pipelines/deployed/new_file.txt") ]
+      expect(Dir[File.join(@updated_git_resource,'coa/pipelines/deployed/') + '*']).to match(expected_files)
+    end
+
+    it 'adds and removes files' do
+      expect(@output).to include('create mode 100644 coa/pipelines/deployed/new_file.txt').and \
+        include(' delete mode 100644 coa/pipelines/deployed/to_be_deleted_file.txt')
+    end
+
+    it 'sets a valid commit message' do
+      repo = Git.open(@updated_git_resource)
+      commit = repo.log.first
+      expect(commit.message).to eq('my commit message')
+    end
+
+  end
+end
