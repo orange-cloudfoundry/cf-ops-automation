05707c277ff2910d1544a5a20755cfab4527f27a
Olivier ORAND <olivier.orand@orange.com>
2018-10-25 12:13:30 +0200

    task(git_update_*): support commit message customization using optional metadata
    this allow us to leverage concourse resource like https://github.com/swce/metadata-resource

--- spec/tasks/git_update_a_file_from_generated/task_spec.rb
+++ spec/tasks/git_update_a_file_from_generated/task_spec.rb
@@ -1,13 +1,10 @@
-# encoding: utf-8
-
 # require 'spec_helper.rb'
 require 'yaml'
 require 'tmpdir'
 require 'fileutils'
 require 'git'
 
-describe 'git_update_a_dir_from_generated task' do
-
+describe 'git_update_a_file_from_generated task' do
   context 'when executed' do
     after(:context) do
       FileUtils.rm_rf @generated_resource
@@ -19,8 +16,7 @@ describe 'git_update_a_dir_from_generated task' do
       @reference_resource = Dir.mktmpdir
       @updated_git_resource = Dir.mktmpdir
       @generated_resource = Dir.mktmpdir
-      File.open(File.join(@generated_resource,'unchanged_file.txt'), 'w') { |file| file.write('Unchanged') }
-      File.open(File.join(@generated_resource,'new_file.txt'), 'w') { |file| file.write('New') }
+      File.open(File.join(@generated_resource, 'updated_file.txt'), 'w') { |file| file.write('updated') }
 
       FileUtils.cp_r(File.join(File.dirname(__FILE__), 'reference-resource', '.'), @reference_resource)
       repo = Git.init(@reference_resource)
@@ -28,22 +24,23 @@ describe 'git_update_a_dir_from_generated task' do
       repo.config('user.email', 'coa-ci@orange.com')
       repo.add
       repo.commit('initialize repository')
-      @output = execute('--include-ignored -c concourse/tasks/git_update_a_dir_from_generated.yml ' \
+      @output = execute('--include-ignored -c concourse/tasks/git_update_a_file_from_generated.yml ' \
         "-i reference-resource=#{@reference_resource} " \
         "-i generated-resource=#{@generated_resource} " \
         "-o updated-git-resource=#{@updated_git_resource} ",
                         'COMMIT_MESSAGE' => "'my commit message'",
-                        'OLD_DIR' => 'coa/pipelines/deployed/')
+                        'OLD_FILE' => 'coa/pipelines/deployed/to_be_updated_file.txt',
+                        'NEW_FILE' => 'updated_file.txt')
     end
 
     it 'updates files in output dir' do
-      expected_files = [File.join(@updated_git_resource,"coa/pipelines/deployed/unchanged_file.txt"), File.join(@updated_git_resource,"coa/pipelines/deployed/new_file.txt") ]
-      expect(Dir[File.join(@updated_git_resource,'coa/pipelines/deployed/') + '*']).to match(expected_files)
+      expected_files = [File.join(@updated_git_resource, "coa/pipelines/deployed/unchanged_file.txt"), File.join(@updated_git_resource, "coa/pipelines/deployed/to_be_updated_file.txt")]
+      expect(Dir[File.join(@updated_git_resource, 'coa/pipelines/deployed/') + '*']).to match(expected_files)
     end
 
-    it 'adds and removes files' do
-      expect(@output).to include('create mode 100644 coa/pipelines/deployed/new_file.txt').and \
-        include(' delete mode 100644 coa/pipelines/deployed/to_be_deleted_file.txt')
+    it 'updates file content' do
+      updated_content = File.read(File.join(@updated_git_resource, "coa/pipelines/deployed/to_be_updated_file.txt"))
+      expect(updated_content).to eq('updated')
     end
 
     it 'sets a valid commit message' do
@@ -52,6 +49,7 @@ describe 'git_update_a_dir_from_generated task' do
       expect(commit.message).to eq('my commit message')
     end
   end
+
   context 'when executed with meta' do
     after(:context) do
       FileUtils.rm_rf @generated_resource
@@ -63,8 +61,7 @@ describe 'git_update_a_dir_from_generated task' do
       @reference_resource = Dir.mktmpdir
       @updated_git_resource = Dir.mktmpdir
       @generated_resource = Dir.mktmpdir
-      File.open(File.join(@generated_resource,'unchanged_file.txt'), 'w') { |file| file.write('Unchanged') }
-      File.open(File.join(@generated_resource,'new_file.txt'), 'w') { |file| file.write('New') }
+      File.open(File.join(@generated_resource, 'updated_file.txt'), 'w') { |file| file.write('updated') }
 
       FileUtils.cp_r(File.join(File.dirname(__FILE__), 'reference-resource', '.'), @reference_resource)
       repo = Git.init(@reference_resource)
@@ -72,30 +69,31 @@ describe 'git_update_a_dir_from_generated task' do
       repo.config('user.email', 'coa-ci@orange.com')
       repo.add
       repo.commit('initialize repository')
-      @output = execute('--include-ignored -c concourse/tasks/git_update_a_dir_from_generated.yml ' \
+      @output = execute('--include-ignored -c concourse/tasks/git_update_a_file_from_generated.yml ' \
         "-i reference-resource=#{@reference_resource} " \
         "-i generated-resource=#{@generated_resource} " \
         "-i concourse-meta=#{File.join(File.dirname(__FILE__), 'meta')} " \
         "-o updated-git-resource=#{@updated_git_resource} ",
                         'COMMIT_MESSAGE' => "'my commit message'",
-                        'OLD_DIR' => 'coa/pipelines/deployed/')
+                        'OLD_FILE' => 'coa/pipelines/deployed/to_be_updated_file.txt',
+                        'NEW_FILE' => 'updated_file.txt')
     end
 
     it 'updates files in output dir' do
-      expected_files = [File.join(@updated_git_resource,"coa/pipelines/deployed/unchanged_file.txt"), File.join(@updated_git_resource,"coa/pipelines/deployed/new_file.txt") ]
-      expect(Dir[File.join(@updated_git_resource,'coa/pipelines/deployed/') + '*']).to match(expected_files)
+      expected_files = [File.join(@updated_git_resource, "coa/pipelines/deployed/unchanged_file.txt"), File.join(@updated_git_resource, "coa/pipelines/deployed/to_be_updated_file.txt")]
+      expect(Dir[File.join(@updated_git_resource, 'coa/pipelines/deployed/') + '*']).to match(expected_files)
     end
 
-    it 'adds and removes files' do
-      expect(@output).to include('create mode 100644 coa/pipelines/deployed/new_file.txt').and \
-        include(' delete mode 100644 coa/pipelines/deployed/to_be_deleted_file.txt')
+    it 'updates file content' do
+      updated_content = File.read(File.join(@updated_git_resource, "coa/pipelines/deployed/to_be_updated_file.txt"))
+      expect(updated_content).to eq('updated')
     end
+
 
     it 'sets a valid commit message' do
       repo = Git.open(@updated_git_resource)
       commit = repo.log.first
       expect(commit.message).to eq("my commit message\n\nCreated by main/pipeline/job/1 - build-name")
     end
-
   end
 end
