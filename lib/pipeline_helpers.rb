afb189929b5402df648de6c3d59b8d7cf5c7f8c6
Olivier ORAND <olivier.orand@orange.com>
2018-11-22 18:01:39 +0100

    pipeline(*): allow concurrent update limitations overriding per root deployment
    related to #184

--- lib/pipeline_helpers.rb
+++ lib/pipeline_helpers.rb
@@ -2,21 +2,28 @@ module PipelineHelpers
   require_relative 'config'
 
   TERRAFORM_CONFIG_DIRNAME_KEY = 'terraform_config'.freeze
+  UNLIMITED_EXECUTION = -1
 
   class << self
     def bosh_io_hosted?(info)
       info["base_location"]&.include?("bosh.io")
     end
 
-    def parallel_execution_limit(config)
-      unlimited_execution = -1
-      result = config.fetch(Config::CONFIG_DEFAULT_KEY, nil)&.fetch(Config::CONFIG_CONCOURSE_KEY, nil)&.fetch(Config::CONFIG_PARALLEL_EXECUTION_LIMIT_KEY, unlimited_execution)
-      result = unlimited_execution if result.nil?
-      result
+    def parallel_execution_limit(config, root_deployment_name = '')
+      extracted_parallel_execution_limit = extract_parallel_execution_limit(config, root_deployment_name) unless root_deployment_name.to_s.empty?
+      extracted_parallel_execution_limit = extract_parallel_execution_limit(config, Config::CONFIG_DEFAULT_KEY) if extracted_parallel_execution_limit.to_s.empty? || extracted_parallel_execution_limit == UNLIMITED_EXECUTION
+      extracted_parallel_execution_limit = UNLIMITED_EXECUTION if extracted_parallel_execution_limit.nil?
+      extracted_parallel_execution_limit
     end
 
-    def enabled_parallel_execution_limit?(config)
-      parallel_execution_limit(config) != -1
+    def enabled_parallel_execution_limit?(config, root_deployment_name = '')
+      parallel_execution_limit(config, root_deployment_name) != UNLIMITED_EXECUTION
+    end
+
+    private
+
+    def extract_parallel_execution_limit(config, root_deployment_name)
+      config.fetch(root_deployment_name, nil)&.fetch(Config::CONFIG_CONCOURSE_KEY, nil)&.fetch(Config::CONFIG_PARALLEL_EXECUTION_LIMIT_KEY, UNLIMITED_EXECUTION)
     end
   end
 end
