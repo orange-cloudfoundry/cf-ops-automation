84fcee12411b926b53926eda6428ea523d137f3e
Lucas Pinto <lucaspinto33@gmail.com>
2019-01-04 11:34:50 +0100

    logger(*): fix the logger so that it works for class methods.

--- lib/concerns/coa_logger.rb
+++ lib/concerns/coa_logger.rb
@@ -2,12 +2,20 @@ require 'logger'
 
 # This utility module helps having a central logging mechanism
 module CoaLogger
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  # this module allows to add class method to a module
+  module ClassMethods
+    def logger
+      logger_path = File.join(File.dirname(__FILE__), '/../..', "log", "stdout.log")
+      Logger.new(logger_path)
+    end
+  end
+
   def logger
-    @logger ||=
-      begin
-        logger_path = File.join(File.dirname(__FILE__), '/../..', "log", "stdout.log")
-        Logger.new(logger_path)
-      end
+    @logger ||= self.class.logger
   end
 end
 
