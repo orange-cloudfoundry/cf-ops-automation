a1b2a41808ec11248028aee8409c65005d183af2
Olivier ORAND <olivier.orand@orange.com>
2019-04-08 12:56:33 +0200

    chore():bump ruby to 2.5.5, gem update, add a check on used docker image
    We switch to ruby 2.5.5 as we were not able to build image based on ubuntu jessie/main packages anymore.
    
    We also bump gem to latest version, including bundler (v 2.0.1)
    
    We also introduce an docker image check: image used by tasks or embedded
    in pipelines are expected.

--- spec/pipelines/static_pipelines_spec.rb
+++ spec/pipelines/static_pipelines_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 require 'tmpdir'
 require 'yaml'
+require_relative '../tasks/task_spec_helper'
 
 describe 'static concourse pipelines spec' do
+  let(:pipelines_dir) { 'concourse/pipelines/' }
+  let(:pipelines_references_fixture) { 'spec/scripts/generate-depls/fixtures/references/' }
+  let(:pipelines_references_dataset) { 'docs/reference_dataset/pipelines/' }
+  let(:pipeline_files) do
+    Dir.glob(pipelines_dir + '**/*.yml') + Dir.glob(pipelines_references_fixture + '*.yml') + Dir.glob(pipelines_references_dataset + '*.yml')
+  end
 
   context 'resource_type exists' do
-    let(:pipelines_dir) { 'concourse/pipelines/'}
-    let(:pipeline_files) { Dir.glob(pipelines_dir + '**/*.yml') } #+ Dir.glob(pipelines_dir + '**/*.erb') }
     let(:resource_types) do
       result = []
       puts "list: #{pipeline_files}"
@@ -34,23 +39,12 @@ describe 'static concourse pipelines spec' do
       end
       images
     end
-    let(:resource_types_object) do
-      result = []
-      puts "list: #{pipeline_files}"
-      pipeline_files.each do |pipeline_filename|
-        puts "processing file #{pipeline_filename}"
-        pipeline = ConcoursePipeline.load_file(pipeline_filename, pipeline_filename).load
-        puts pipeline.resource_types
-        result.concat(pipeline.resource_types.to_a) unless pipeline.resource_types.empty?
-      end
-      result.uniq
-    end
 
     it 'founds resource_type' do
       expect(resource_types).not_to be_empty
     end
 
-    it 'uses an existing image on docker hub'do
+    it 'uses an existing image on docker hub' do
       docker_images_from_resource_type.each do |image, files|
         puts "processing image #{image}"
         parsed_image = image.split(':')
@@ -60,11 +54,67 @@ describe 'static concourse pipelines spec' do
         begin
           manifest = docker_registry.manifest(docker_image, docker_image_tag)
           expect(manifest).not_to be_empty
-          rescue DockerRegistry2::NotFound => not_found
-            raise DockerRegistry2::NotFound, " #{image} used by #{files.to_s}"
+        rescue DockerRegistry2::NotFound => not_found
+          raise DockerRegistry2::NotFound, " #{image} used by #{files.to_s}"
         end
       end
     end
+  end
 
+  context 'when job image_resources is defined' do
+    let(:expected_task_images) do
+      [{ "repository" => "concourse/busyboxplus", "tag" => "git" },
+       { "repository" => TaskSpecHelper.ruby_image, "tag" => TaskSpecHelper.ruby_image_version },
+       { "repository" => "orangecloudfoundry/orange-cf-bosh-cli" },
+       { "repository" => TaskSpecHelper.ruby_image, "tag" => TaskSpecHelper.ruby_slim_image_version },
+       { "repository" => TaskSpecHelper.terraform_image, "tag" => TaskSpecHelper.terraform_image_version },
+       { "repository" => "alpine", "tag" => "3.5" },
+       { "repository" => "governmentpaas/curl-ssl" },
+       { "repository" => TaskSpecHelper.bosh_cli_v2_image, "tag" => TaskSpecHelper.bosh_cli_v2_image_version }]
+    end
+    let(:image_resources) do
+      result = []
+      puts "list: #{pipeline_files}"
+      pipeline_files.each do |pipeline_filename|
+        puts "processing file #{pipeline_filename}"
+        pipeline = YAML.load_file(pipeline_filename)
+        image_resources = pipeline['jobs']&.flat_map { |job| job['plan'] }&.compact&.flat_map { |config| config['config'] }&.compact&.flat_map { |image_resource| image_resource['image_resource'] }
+        sources = image_resources&.map { |job| job['source'] }
+        result.concat(sources.uniq) if sources
+      end
+      result.uniq
+    end
+    let(:docker_images_from_image_resource_jobs) do
+      images = []
+      image_resources.each do |image_resource|
+        puts "processing task config #{image_resource}"
+        docker_image = image_resource['repository'].to_s
+        docker_image = "library/#{docker_image}" unless docker_image.include?('/')
+        docker_image_tag = image_resource['tag'] || 'latest'
+        name = "#{docker_image}:#{docker_image_tag}"
+        images << name
+      end
+      images.sort
+    end
+
+    it 'matches expected images' do
+      expect(image_resources).to match_array(expected_task_images)
+    end
+
+    it 'exists on docker hub' do
+      docker_images_from_image_resource_jobs.each do |image|
+        puts "processing image #{image}"
+        parsed_image = image.split(':')
+        docker_image = parsed_image.first
+        docker_image_tag = parsed_image.last
+        puts "searching for #{docker_image}:#{docker_image_tag}"
+        begin
+          manifest = docker_registry.manifest(docker_image, docker_image_tag)
+          expect(manifest).not_to be_empty
+        rescue DockerRegistry2::NotFound => not_found
+          raise DockerRegistry2::NotFound, " #{image} not found"
+        end
+      end
+    end
   end
 end
--- spec/tasks/all_tasks_spec.rb
+++ spec/tasks/all_tasks_spec.rb
@@ -13,7 +13,7 @@ describe 'all tasks' do
       tasks.each do |task_filename|
         puts "processing file #{task_filename}"
         task = YAML.load_file (task_filename)
-        docker_image = task['image_resource']&.fetch('source',[])&.fetch('repository',[]).to_s
+        docker_image = task['image_resource']&.fetch('source', [])&.fetch('repository', []).to_s
         if docker_image.empty?
           puts "No image detected, ignoring #{task_filename}"
           next
@@ -21,30 +21,39 @@ describe 'all tasks' do
         docker_image = "library/#{docker_image}" unless docker_image.include?('/')
         docker_image_tag = task['image_resource']['source']['tag'] || 'latest'
         name = "#{docker_image}:#{docker_image_tag}"
-        if images[name]
-          images[name] = images[name]<< task_filename
-        else
-          images[name] = [task_filename]
-        end
+        images[name] = if images[name]
+                         images[name] << task_filename
+                       else
+                         [task_filename]
+                        end
       end
       images
+    end
+    let(:expected_task_images) do
+      [
+        "concourse/buildroot:curl",
+        "concourse/busyboxplus:git",
+        TaskSpecHelper.bosh_cli_v2_image + ':' + TaskSpecHelper.bosh_cli_v2_image_version,
+        "governmentpaas/cf-cli:latest",
+        "governmentpaas/spruce:latest",
+        "library/ruby:" + TaskSpecHelper.ruby_image_version,
+        "library/ruby:" + TaskSpecHelper.ruby_slim_image_version,
+        "orangecloudfoundry/cf-ops-automation:latest",
+        "orangecloudfoundry/spiff:latest",
+        TaskSpecHelper.terraform_image + ':' + TaskSpecHelper.terraform_image_version
+      ]
     end
 
     it 'ensures some tasks exist' do
       expect(tasks).not_to be_empty
     end
 
+    it 'x' do
+      puts docker_images_with_task.keys.sort
+      expect(docker_images_with_task.keys).to match_array(expected_task_images)
+    end
+
     it 'uses an existing image on docker hub'do
-      # tasks.each do |task_filename|
-      #   puts "processing #{task_filename}"
-      #   task = YAML.load_file (task_filename)
-      #   docker_image = task['image_resource']['source']['repository'].to_s
-      #   docker_image = "library/#{docker_image}" unless docker_image.include?('/')
-      #   docker_image_tag = task['image_resource']['source']['tag']|| 'latest'
-      #   puts "searching for #{docker_image}:#{docker_image_tag}"
-      #   manifest = docker_registry.manifest(docker_image, docker_image_tag)
-      #   expect(manifest).not_to be_empty
-      # end
       docker_images_with_task.each do |image, files|
         puts "processing image #{image}"
         parsed_image = image.split(':')
@@ -52,10 +61,10 @@ describe 'all tasks' do
         docker_image_tag = parsed_image[1]
         puts "searching for #{docker_image}:#{docker_image_tag}"
         begin
-        manifest = docker_registry.manifest(docker_image, docker_image_tag)
-        expect(manifest).not_to be_empty
-        rescue DockerRegistry2::NotFound => not_found
-          raise DockerRegistry2::NotFound, " #{image} used by #{files.to_s}"
+          manifest = docker_registry.manifest(docker_image, docker_image_tag)
+          expect(manifest).not_to be_empty
+          rescue DockerRegistry2::NotFound => not_found
+            raise DockerRegistry2::NotFound, " #{image} used by #{files.to_s}"
         end
       end
     end
