fd0fc060d886c78ab09493e9ade567c37cfb1c68
Olivier ORAND <olivier.orand@orange.com>
2019-04-26 17:36:58 +0200

    pipeline(*): support private docker registry
    This require new secret, to describe docker registry endpoint:
     - docker-registry-url: <host>:<port>/
     This variable must be present to use a private registry, or empty to use dockerhub
    
    close #254

--- spec/lib/template_processor/template_processor_for_cf_apps_pipeline_spec.rb
+++ spec/lib/template_processor/template_processor_for_cf_apps_pipeline_spec.rb
@@ -62,7 +62,7 @@ describe 'CfAppsPipelineTemplateProcessing' do
       - name: slack-notification
         type: docker-image
         source:
-          repository: cfcommunity/slack-notification-resource
+          repository: ((docker-registry-url))cfcommunity/slack-notification-resource
           tag: v1.4.2
     YAML
     YAML.safe_load(resource_types_yaml)
