8ca9d02576e130ca63c4a75dfbe7a030ab0f82f5
Olivier ORAND <olivier.orand@orange.com>
2019-05-15 10:30:06 +0200

    test(logger): fix relative path detection

--- spec/lib/coa/utils/coa_logger_spec.rb
+++ spec/lib/coa/utils/coa_logger_spec.rb
@@ -7,12 +7,13 @@ describe Coa::Utils::CoaLogger do
     let(:coa_logger) { Class.new { include Coa::Utils::CoaLogger }.logger }
     let(:logger_tmpdir) { Dir.mktmpdir('coa_logger_') }
     let(:log_filepath) { logger_tmpdir }
-    let(:log_file_pathname) { Pathname.new(log_filepath) }
-    let(:log_file_relative_path) { log_file_pathname.relative_path_from(current_pathname) }
+    let(:root_path) { Pathname.new('/') }
     let(:current_pathname) { Pathname.new(File.dirname(__FILE__)) }
+    let(:current_relative_pathname) { root_path.relative_path_from(current_pathname) }
+    let(:coa_log_path) { File.join current_relative_pathname, log_filepath }
 
     it 'creates log file at this path' do
-      allow(ENV).to receive(:fetch).with("COA_LOG_PATH", "../../../log").and_return(log_file_relative_path)
+      allow(ENV).to receive(:fetch).with("COA_LOG_PATH", "../../../log").and_return(coa_log_path)
       coa_logger.log_and_puts :info, 'my info message'
 
       expect(File).to exist(log_filepath)
