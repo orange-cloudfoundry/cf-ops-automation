3c1704fb52dc1250d7f0ca72f0319f36588ee458
Olivier ORAND <olivier.orand@orange.com>
2019-06-25 18:05:39 +0200

    chore(pipeline): trigger IT on image changes
    [skip ci]

--- ci/load-pipeline.sh
+++ ci/load-pipeline.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+FLY_CMD=${FLY_CMD:-fly}
+FLY_TARGET=${FLY_TARGET:-cf-ops-automation}
+CREDENTIAL_FILE=${CREDENTIAL_FILE:-private.yml}
 
-fly -t cf-ops-automation set-pipeline -p cf-ops-automation -c pipeline.yml -l private.yml
+
+${FLY_CMD} -t ${FLY_TARGET} set-pipeline -p cf-ops-automation -c pipeline.yml -l ${CREDENTIAL_FILE}
