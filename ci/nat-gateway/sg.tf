a60d949ceb7815aa271dd55d2d433031527ea7ca
Olivier ORAND <olivier.orand@orange.com>
2019-07-12 11:56:50 +0200

    fix sg tf name

--- ci/nat-gateway/sg.tf
+++ ci/nat-gateway/sg.tf
@@ -1,11 +1,11 @@
-data "openstack_networking_secgroup_v2" "secgroup" {
+data "openstack_networking_secgroup_v2" "tf-default-sg" {
   name = "tf-default-sg"
 }
 
-data "openstack_networking_secgroup_v2" "secgroup" {
+data "openstack_networking_secgroup_v2" "bootstrap-sg" {
   name = "bootstrap-sg"
 }
 
-data "openstack_networking_secgroup_v2" "secgroup" {
+data "openstack_networking_secgroup_v2" "coa-sg" {
  name = "coa-sg"
 }
