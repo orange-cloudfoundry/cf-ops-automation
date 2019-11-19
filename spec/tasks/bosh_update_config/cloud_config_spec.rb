3f585938c00ce3804dce9ce41f590e9540ab687a
Olivier ORAND <olivier.orand@orange.com>
2019-11-19 17:22:47 +0100

    feature(support-cpi-file): handle cpi config
    we refactor cloud and runtime config tasks to share the same task between
     cpi, cloud and runtime config.
    
    close #265
    close #191

--- spec/tasks/bosh_update_config/cloud_config_spec.rb
+++ spec/tasks/bosh_update_config/cloud_config_spec.rb
@@ -1,41 +1,37 @@
 require 'spec_helper'
 require 'yaml'
 require 'tmpdir'
+require_relative 'test_config_generator'
 
 describe 'bosh_update_cloud_config task' do
   context 'when no bosh is available' do
 
     before(:context) do
-      @config_manifest = Dir.mktmpdir
+      @test_config_generator = TestConfigGenerator.new
       @secrets = Dir.mktmpdir
-
-      FileUtils.touch(File.join(@config_manifest, 'my-custom-cloud-vars.yml'))
-      FileUtils.touch(File.join(@config_manifest, '01-my-custom-cloud-operators.yml'))
-      FileUtils.touch(File.join(@config_manifest, '02-my-custom-cloud-operators.yml'))
-      FileUtils.touch(File.join(@config_manifest, 'my-custom-runtime-vars.yml'))
-      FileUtils.touch(File.join(@config_manifest, '01-my-custom-runtime-operators.yml'))
-      FileUtils.touch(File.join(@config_manifest, '02-my-custom-runtime-operators.yml'))
 
       fly_cli_environment = {
         'BOSH_TARGET' => 'https://dummy-bosh',
         'BOSH_CLIENT' => 'aUser',
         'BOSH_CLIENT_SECRET' => 'aPassword',
-        'BOSH_CA_CERT' => 'secrets/shared/certs/internal_paas-ca/server-ca.crt'
+        'BOSH_CA_CERT' => 'secrets/shared/certs/internal_paas-ca/server-ca.crt',
+        'CONFIG_TYPE' => 'cloud'
       }
 
-      @output = execute('-c concourse/tasks/bosh_update_cloud_config.yml ' \
+      @output = execute('-c concourse/tasks/bosh_update_config/task.yml ' \
         '-i scripts-resource=. ' \
         "-i secrets=#{@secrets} " \
-        "-i config-manifest=#{@config_manifest} ", \
+        "-i config-manifest=#{@test_config_generator.config_manifest_path} ", \
         fly_cli_environment )
     rescue FlyExecuteError => e
+      puts "OUTPUT: #{e.out}"
       @output = e.out
       @fly_error = e.err
       @fly_status = e.status
     end
 
     after(:context) do
-      FileUtils.rm_rf @config_manifest if File.exist?(@config_manifest)
+      @test_config_generator.cleanup
       FileUtils.rm_rf @secrets if File.exist?(@secrets)
     end
 
@@ -55,13 +51,17 @@ describe 'bosh_update_cloud_config task' do
       expect(@output).to include("no address for dummy-bosh")
     end
 
+    it 'returns no message on stderr' do
+      expect(@fly_error).to eq('')
+    end
+
     it 'returns with exit status 1' do
       expect(@fly_status.exitstatus).to eq(1)
     end
   end
 
   context 'Pre-requisite' do
-    let(:task) { YAML.load_file 'concourse/tasks/bosh_update_cloud_config.yml' }
+    let(:task) { YAML.load_file 'concourse/tasks/bosh_update_config/task.yml' }
 
     it 'uses alphagov bosh-cli-v2 image' do
       docker_image_used = task['image_resource']['source']['repository'].to_s
