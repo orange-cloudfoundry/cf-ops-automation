6ccab14ed2904ff925c6bbd0ff71e92021aeec9c
Olivier ORAND <olivier.orand@orange.com>
2019-11-27 16:24:51 +0100

    chore(ci): refactor load pipeline script to include a pre load spruce processing
    so we can define, only diff in a dedicated file instead of duplicate the whole private config

--- ci/load-pipeline.sh
+++ ci/load-pipeline.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 FLY_CMD=${FLY_CMD:-fly}
 FLY_TARGET=${FLY_TARGET:-cf-ops-automation}
-CREDENTIAL_FILE=${CREDENTIAL_FILE:-private.yml}
+DEFAULT_CREDENTIAL_FILE="private.yml"
+CREDENTIAL_FILE=${CREDENTIAL_FILE:-$DEFAULT_CREDENTIAL_FILE}
+DIFF_FILE="${CREDENTIAL_FILE%%-generated.yml}-diff.yml"
 
-
-${FLY_CMD} -t ${FLY_TARGET} set-pipeline -p cf-ops-automation -c pipeline.yml -l ${CREDENTIAL_FILE}
+if [[ "$CREDENTIAL_FILE" != "$DEFAULT_CREDENTIAL_FILE" ]];then
+    touch "${DIFF_FILE}"
+    echo "Using diff: $DIFF_FILE"
+    spruce merge private.yml "${DIFF_FILE}" > "${CREDENTIAL_FILE}"
+    if [ $? -ne 0 ];then
+        exit $?
+    fi
+fi
+${FLY_CMD} -t "${FLY_TARGET}" set-pipeline -p cf-ops-automation -c pipeline.yml -l "${CREDENTIAL_FILE}"
