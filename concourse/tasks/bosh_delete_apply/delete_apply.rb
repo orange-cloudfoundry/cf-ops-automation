043c9b2d08eb16d68d055f64fb3c07ec9c54e4c9
Olivier ORAND <olivier.orand@orange.com>
2020-02-26 11:50:02 +0100

    feature(bosh-delete-deployment): fail-slow, to delete as much deployments as possible
    to avoid leaks, we choose to ignore errors on bosh delete, so we can delete as much deployments as possible and we get all errors at the end.

--- concourse/tasks/bosh_delete_apply/delete_apply.rb
+++ concourse/tasks/bosh_delete_apply/delete_apply.rb
@@ -9,6 +9,29 @@ class DeleteApply
   end
 
   def process
+    deployed_bosh_deployments = filter_deployment_to_delete
+
+    deployment_deletion_failure = delete_deployments(deployed_bosh_deployments)
+    if deployment_deletion_failure.empty?
+      @config_repo_deployments.cleanup_disabled_deployments
+    else
+      deployment_deletion_failure.each { |deployment_name, error| puts "Error deleting #{deployment_name}: #{error}" }
+      raise Tasks::Bosh::BoshCliError, "Failed to delete deployments: #{deployment_deletion_failure.keys}"
+    end
+  end
+
+  private
+
+  def delete_deployments(deployed_bosh_deployments)
+    puts "Deployments to delete: #{deployed_bosh_deployments}"
+    deployment_deletion_failure = {}
+    deployed_bosh_deployments.each do |deployment_name|
+      delete_deployment(deployment_deletion_failure, deployment_name)
+    end
+    deployment_deletion_failure
+  end
+
+  def filter_deployment_to_delete
     expected_deployments = @config_repo_deployments.enabled_deployments
     puts "Expected deployments: #{expected_deployments}"
     protected_deployments = @config_repo_deployments.protected_deployments
@@ -18,12 +41,13 @@ class DeleteApply
     puts "Active bosh deployments: #{deployed_bosh_deployments}"
     puts "Filtering deployments (ie: excluding expected and protected deployments)"
     deployed_bosh_deployments.delete_if { |deployment_name| expected_deployments&.include?(deployment_name) || protected_deployments&.include?(deployment_name) }
+    deployed_bosh_deployments
+  end
 
-    puts "Deployments to delete: #{deployed_bosh_deployments}"
-    deployed_bosh_deployments.each do |deployment_name|
-      @delete_command_holder.execute(deployment_name)
-      @config_repo_deployments.cleanup_deployment(deployment_name)
-    end
-    @config_repo_deployments.cleanup_disabled_deployments
+  def delete_deployment(deployment_deletion_failure, deployment_name)
+    @delete_command_holder.execute(deployment_name)
+    @config_repo_deployments.cleanup_deployment(deployment_name)
+  rescue Tasks::Bosh::BoshCliError => e
+    deployment_deletion_failure[deployment_name] = e
   end
 end
