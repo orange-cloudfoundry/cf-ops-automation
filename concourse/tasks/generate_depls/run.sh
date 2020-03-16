223e7c1c589828d5bd6238aa3f35b05708ea817a
Olivier ORAND <olivier.orand@orange.com>
2020-03-16 14:52:57 +0100

    doc(copyright): update for 2020

--- concourse/tasks/generate_depls/run.sh
+++ concourse/tasks/generate_depls/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2015-2018 Orange
+# Copyright (C) 2015-2020 Orange
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
