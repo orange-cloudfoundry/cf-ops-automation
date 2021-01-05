2d344703d57767bc9098f896d599f9cb3572950e
Olivier ORAND <olivier.orand@orange.com>
2021-01-05 19:06:02 +0100

    feature(tune-cf-push-options): allow cf push customization
    With cf cli v7 support, we switch to "rolling strategy" to avoid application downtime. But "--strategy" in not compliant with "-f" option when manifest includes multiple app definitions. So we add a mechanism to override cf push default parameters.
    
    See #240

---
+++ docs/reference_dataset/template_repository/hello-world-root-depls/cf-apps-deployments/generic-app/template/cf-env.sh
@@ -0,0 +1,4 @@
+#
+# CF_PUSH_OPTIONS= # Use this syntax to avoid using cf push default parameters
+
+CF_PUSH_OPTIONS="${CF_PUSH_OPTIONS} --app-start-timeout 30" # Use this syntax to complete cf push default parameters
