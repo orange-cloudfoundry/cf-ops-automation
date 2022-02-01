ea6cdbfd6f844ba285903f87268f4bd1fc73b3c7
Olivier ORAND <olivier.orand@orange.com>
2022-02-01 18:47:25 +0100

    pipeline(control-plane): bump concourse-micro-legacy
    We do not require old concourse 5.x authentication mechanism anymore. So we use identical configuration for concourse-micro and concourse-micro-legacy resources. We comment 'save-deployed-pipelines-legacy' to avoid useless API calls, and to be able to reintroduce it when required.

---
+++ spec/pipelines/control_plane_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'tmpdir'
+require 'yaml'
+require_relative '../tasks/task_spec_helper'
+
+describe 'control plane pipeline spec' do
+  let(:pipeline_file) { File.join('concourse', 'pipelines', 'control-plane.yml') }
+  let(:control_plane_pipeline) { YAML.load_file(pipeline_file) }
+  let(:control_plane_jobs) { control_plane_pipeline['jobs'] }
+  let(:control_plane_resources) { control_plane_pipeline['resources'] }
+
+  context 'when checking pipelines definition' do
+    let(:expected_jobs) { %w[detect-paas-templates-scanned-changes load-generated-pipelines manual-reset-avoid-please manual-setup on-git-commit push-changes reset-secrets-pipeline-generation save-deployed-pipelines] }
+
+    it 'has expected jobs' do
+      jobs = control_plane_jobs.flat_map { |item| item['name'] }.sort
+      expect(jobs).to eq(expected_jobs)
+    end
+
+    it 'has expected  resources' do
+      expected_resources = %w[cf-ops-automation concourse-meta concourse-micro concourse-micro-legacy failure-alert paas-templates-full paas-templates-scanned paas-templates-versions secrets-generated-pipelines secrets-writer]
+      resources = control_plane_resources.flat_map { |item| item['name'] }.sort
+      expect(resources).to eq(expected_resources)
+    end
+  end
+end
