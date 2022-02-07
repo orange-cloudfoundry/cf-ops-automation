b929d0d715c8e3fb8d016fd5052a1caf84a40d1d
m0dulev1 <73461919+m0dulev1@users.noreply.github.com>
2022-02-07 15:39:07 -0800

    Delete features directory

--- features/support/env.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../../lib/coa/reference_dataset_documentation'
-require_relative '../../lib/coa/constants'
-
-puts '_____________________'
-puts 'Before running COA tests, we remove all generated pipelines from output_dir'
-puts '_____________________'
-
-Coa::ReferenceDatasetDocumentation::DocsConfig.
-  cleanup_generated_pipelines(Coa::Constants::REFERENCE_DATASET_PATH)
