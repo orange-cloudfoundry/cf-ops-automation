96b9d50b738ca490e012bea2e85d8bc5f863a3d7
Olivier ORAND <olivier.orand@orange.com>
2022-09-19 16:56:15 +0200

    tasks(generate_*): fix spec

--- spec/generated_templates_helper.rb
+++ spec/generated_templates_helper.rb
@@ -1,12 +1,13 @@
 class GeneratedTemplatesHelper
   attr_reader :root_dir, :root_deployment_name
 
-  def initialize(root_dir, root_deployment_name: 'hello-world-root-depls', ignore_templates_pipelines: false)
+  def initialize(root_dir, root_deployment_name: 'hello-world-root-depls', ignore_shared_pipelines: true, ignore_templates_pipelines: false)
     @root_dir = root_dir
     @template_pipeline_dir = "#{@root_dir}/concourse/pipelines/template/*.erb"
     @shared_pipeline_dir = "#{@root_dir}/concourse/pipelines/shared/*.erb"
     @root_deployment_name = root_deployment_name
     @ignore_templates_pipelines = ignore_templates_pipelines
+    @ignore_shared_pipelines = ignore_shared_pipelines
     erb_pipelines
   end
 
@@ -19,17 +20,22 @@ class GeneratedTemplatesHelper
   end
 
   def generated_pipelines
-    pipelines_from_shared = @shared_pipelines.map do |name|
-      new_name = name.gsub('-pipeline.yml.erb', '-generated.yml')
-      'shared-' + new_name
-    end
-    pipelines_from_shared + (@ignore_templates_pipelines ? [] : add_generated_templates_pipelines)
+    pipelines = []
+    pipelines + add_generated_shared_pipelines unless @ignore_shared_pipelines
+    pipelines + add_generated_templates_pipelines unless @ignore_templates_pipelines
   end
 
   private
 
+  def add_generated_shared_pipelines
+    @shared_pipelines.map do |name|
+      new_name = name.gsub('-pipeline.yml.erb', '-generated.yml')
+      'shared-' + new_name
+    end
+  end
+  JSON.dump
   def add_generated_templates_pipelines
-    pipelines_from_templates = @templates_pipelines.map do |name|
+    @templates_pipelines.map do |name|
       new_name = name.gsub('-pipeline.yml.erb', '-generated.yml')
       @root_deployment_name + '-' + new_name
     end
