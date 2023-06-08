2abc96c78edcc6e075f97565c8ba6ea99f420956
Olivier ORAND <olivier.orand@orange.com>
2023-06-08 12:09:34 +0000

    chore(renovate): tune PR grouping
    [ci skip]

--- run-renovate-locally.bash
+++ run-renovate-locally.bash
@@ -5,12 +5,16 @@ LOG_LEVEL="${LOG_LEVEL:-debug}"
 RENOVATE_ENABLED_MANAGERS="${RENOVATE_ENABLED_MANAGERS:-""}"
 RENOVATE_INCLUDE_PATHS="${RENOVATE_INCLUDE_PATHS:-""}"
 if [ -z "$GITHUB_COM_TOKEN" ];then
-  echo -e "WARNING: missing GitHub token to allow. Please set it before running this script, using \n export GITHUB_COM_TOKEN=\"xxx\""
+  echo -e "WARNING: missing GitHub token to allow github release version detection. Please set it before running this script, using \n export GITHUB_COM_TOKEN=\"xxx\""
   sleep 1
 fi
-echo "Log level: $LOG_LEVEL"
-echo "RENOVATE_ENABLED_MANAGERS: $RENOVATE_ENABLED_MANAGERS #Empty means all managers enabled"
-echo "RENOVATE_INCLUDE_PATHS: $RENOVATE_INCLUDE_PATHS #Empty means scan all paths"
+if [ -z "$RENOVATE_BOT" ];then
+  echo -e "WARNING: missing Renovate Bot. Please set it before running this script, using \n export RENOVATE_BOT=\"xxx\""
+  sleep 1
+fi
+echo "Set LOG_LEVEL to manage log level. Default 'debug'.Current Log level: <$LOG_LEVEL>"
+echo "Set RENOVATE_ENABLED_MANAGERS to restrict active managers. Current RENOVATE_ENABLED_MANAGERS: <$RENOVATE_ENABLED_MANAGERS> #Empty means all managers are enabled"
+echo "Set RENOVATE_INCLUDE_PATHS to restrict renovate scan. Current RENOVATE_INCLUDE_PATHS: <$RENOVATE_INCLUDE_PATHS> #Empty means scan all paths"
 echo "Git repo volume path: $GIT_REPO"
 docker run \
     --rm \
@@ -18,10 +22,12 @@ docker run \
     -e GITHUB_COM_TOKEN="$GITHUB_COM_TOKEN" \
     -e RENOVATE_ENABLED_MANAGERS="$RENOVATE_ENABLED_MANAGERS" \
     -e RENOVATE_INCLUDE_PATHS="$RENOVATE_INCLUDE_PATHS" \
+    -e RENOVATE_BOT="$RENOVATE_BOT" \
     -v "$GIT_REPO:/tmp/local-git-repo" \
     --workdir /tmp/local-git-repo \
     ghcr.io/renovatebot/renovate \
     --platform=local \
     --semantic-commits=disabled \
-    | tee -a renovate.log
+| tee renovate.log
+
 #    --dry-run="true" \
