a343883bf3c03f55d9b4ba02f9c6d95ec6704892
Olivier ORAND <olivier.orand@orange.com>
2023-10-25 11:58:16 +0000

    test(integration-tests): polish log message
    Display info about log file and executed command to run tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,7 @@ require_relative '../../lib/coa/constants'
 
 puts '_____________________'
 puts 'Before running COA tests, we remove all generated pipelines from output_dir'
+puts "Reminder: \e[31mLogs are available at './coa_run_stdout.log'\e[0m"
 puts '_____________________'
 
 Coa::ReferenceDatasetDocumentation::DocsConfig.
